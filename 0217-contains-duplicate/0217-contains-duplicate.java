1aeea687ec8a9e9d722192bc31b20a5f7d4d002f
Sisira07 <asapu.sisira@gmail.com>
2026-08-15 22:00:03 +0530

    Time: 18 ms (70.05%), Space: 108.3 MB (31.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,9 +3,9 @@ class Solution {
         HashSet<Integer> set=new HashSet<>();
 
         for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])) return true;
             set.add(nums[i]);
         }
-        if(set.size()!=nums.length) return true;
         return false;
     }
 }
